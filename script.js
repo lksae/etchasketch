8daa7a9d5042fc7c8c34d3284fcf9eb1f9338f1f
Lksae <matthiasschmitz21@gmail.com>
2022-08-20 15:25:38 +0200

    Finish hovering effect on grid

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-for (let i = 1; i < 16; i++) {
+function makeBlack(){
+    flexItem.classList.add('black');
+}
+
+function makeNormal(){
+    flexItem.classList.remove('black');
+}
+
+
+for (let j = 1; j < 17; j++) {
     const container = document.querySelector('#container');
 
     const flex = document.createElement('div');
@@ -7,14 +16,23 @@ for (let i = 1; i < 16; i++) {
     
     container.appendChild(flex);
 
-    for (let i = 1; i <16; i++) {
+    for (let i = 1; i <17; i++) {
         const flexContainer = document.querySelector('#flex');
         const flexItem = document.createElement('div');
+        //flexItem.onmouseover = makeBlack;
+        //flexItem.onmouseout = makeNormal;
         flexItem.classList.add('flexItem');
         //flexItem.textContent = 'flex'
 
         flex.appendChild(flexItem);
 
     }
+
+
 }
+/*const flexItems = document.querySelectorAll(".flexItem");
+flexItems.forEach(flexI => flexI.addEventListener("mouseover", makeBlack));*/
+
+/*const keys = document.querySelectorAll(".key");
+keys.forEach( key => key.addEventListener("transitionend", removeTransition));*/
 
